3c28bfd31b79489bf320e7a6c1261d09a7a628d6
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-04-24 18:07:18 -0700

    Added basic Gnod behavior

---
+++ Assets/Scripts/GnodController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GnodController : MonoBehaviour
+{
+    public Transform follow;
+    public float moveSpeed = 1f;
+    public float rotationSpeed = 3f;
+
+    private Vector3 direction;
+    private Quaternion lookRot;
+
+    void Update()
+    {
+        direction = (follow.position - transform.position).normalized;
+        lookRot = Quaternion.LookRotation(direction) * Quaternion.Euler(90, 0, 0);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * rotationSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, follow.position, Time.deltaTime * moveSpeed);
+    }
+}
