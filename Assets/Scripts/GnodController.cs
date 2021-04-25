db51b5735ce6f87ae35381fa7b9c6a4a761c1416
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-04-24 20:35:32 -0700

    Added audio cue

--- Assets/Scripts/GnodController.cs
+++ Assets/Scripts/GnodController.cs
@@ -5,11 +5,28 @@ using UnityEngine.AI;
 
 public class GnodController : MonoBehaviour
 {
-    public Transform agent;
+    public GnodNavAgent agent;
+    public float maxPitch = 1.4f;
+    public float maxDist = 20f;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void FixedUpdate()
     {
-        transform.rotation = agent.rotation * Quaternion.Euler(90, 0, 0);
-        transform.position = new Vector3(agent.position.x, agent.position.y + 3, agent.position.z);
+        transform.rotation = agent.transform.rotation * Quaternion.Euler(90, 0, 0);
+        transform.position = new Vector3(agent.transform.position.x, agent.transform.position.y + 3, agent.transform.position.z);
+
+        float distance = (agent.follow.position - transform.position).magnitude;
+        if (distance < maxDist) {
+            float distPercent = 1.0f - (distance/maxDist);
+            audioSource.pitch = 1.0f + (distPercent * (maxPitch - 1.0f));
+        } else {
+            audioSource.pitch = 1.0f;
+        }
     }
 }
